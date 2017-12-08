cecbad13fd8fda0a25def751f2919ebcc474cebd
RaulVinueza <raulmvinueza@gmail.com>
2017-12-08 12:54:24 -0600

    Campus queries working now

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -1,24 +1,54 @@
 const router = require('express').Router()
 const db = require('../db')
+const Campus = require('../db/models/Campus')
 
 router.get('/', (req, res, next) => {
-    res.json({reply: 'getting all the campuses'})
+    Campus.findAll()
+    .then( campuses => res.send(campuses))
+    .catch(next)
 })
 
 router.get('/:id', (req, res, next) => {
-    res.json({campusSelected: req.params.id})
+    Campus.findById(req.params.id)
+    .then(campus => res.send(campus))
+    .catch(next)
 })
 
 router.post('/', (req, res, next) => {
-    res.json(req.body)
+    Campus.create({
+        name: req.body.name,
+        imageURL: req.body.imageURL,
+        description: req.body.description
+    }).then( campus => res.send(campus))
+    .catch(next)
 })
 
 router.put('/:id', (req, res, next) => {
-    res.json(req.body)
+    Campus.update({
+        name: req.body.name,
+        imageURL: req.body.imageURL,
+        description: req.body.description
+    }, {
+        where: {id: req.params.id}
+    })
+    .then(() => res.sendStatus(202))
+    .catch(next)
 })
 
 router.delete('/:id', (req, res, next) => {
-    res.json(req.body)
+    Campus.destroy({
+        where: {id: req.params.id}
+    })
+    .then(() => res.sendStatus(202))
+    .catch(next)
 })
 
 module.exports = router
+
+//for testing
+
+//  {
+//     "name": "Chicago",
+//     "imageURL": "http://lorempixel.com/400/200",
+//     "description": "this is a description"
+//  }
